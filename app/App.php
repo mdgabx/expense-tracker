58b40e0f2ad115b43bbf90c165238c248b23ae8b
Dex <a.markdexter@gmail.com>
2025-03-25 21:20:03 +0800

    adding path and transaction files

--- app/App.php
+++ app/App.php
@@ -1,3 +1,19 @@
 <?php
 
 declare(strict_types = 1);
+
+// code
+function getTransactionsFiles(): array {
+	$files = [];
+
+
+	foreach(scandir(FILES_PATH) as $file) {
+		if(is_dir($file)) {
+			continue;
+		}
+
+		$files[] = $file;
+	}
+
+	return $files;
+}
--- public/index.php
+++ public/index.php
@@ -1,3 +1,18 @@
 <?php
 
-echo "hello world";
+declare(strict_types = 1);
+
+var_dump(DIRECTORY_SEPARATOR);
+
+$root = dirname(__DIR__) . DIRECTORY_SEPARATOR;
+
+define('APP_PATH', $root . 'app' . DIRECTORY_SEPARATOR);
+define('FILES_PATH', $root . 'transaction_files' . DIRECTORY_SEPARATOR);
+define('VIEWS_PATH', $root . 'views' . DIRECTORY_SEPARATOR);
+
+
+require APP_PATH . "App.php";
+
+$files = getTransactionsFiles();
+
+var_dump($files);
